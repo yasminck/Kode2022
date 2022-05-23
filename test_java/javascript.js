4049dd21f4e29de6062fa804381801827113bd32
yasminck <91217979+yasminck@users.noreply.github.com>
2022-05-23 11:39:27 +0200

    Add files via upload

---
+++ test_java/javascript.js
@@ -0,0 +1,14 @@
+
+function tak () {
+    alert ("Husk at svare på alle spørgesmål!")
+}
+
+function tak1 () {
+    alert ("Tak for svaret!")
+}
+
+
+function svar () {
+const q1 = document.getElementsByName ("input");
+console.log(q1.inner);
+}
